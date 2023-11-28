cd68ba29567208f9b146460e8ce10a6b649dc74b
Syed Wajahat Ali <wajahat.klk25.com>
2023-11-28 00:02:40 +0000

    adds componnents to auth

--- src/pages/auth.tsx
+++ src/pages/auth.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Login from "../components/Login";
+import Loutgout from "../components/Loutgout";
 
 const Auth = () => {
-  return <div>auth</div>;
+  return (
+    <div>
+      <Login />
+      <Loutgout />
+    </div>
+  );
 };
 
 export default Auth;
