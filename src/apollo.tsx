dc9746e562e8e7902dd12842d968437b8596a21c
Soufiane Bdaoui <soufianebdaoui@gmail.com>
2023-12-02 02:21:28 +0100

    working product adding

--- src/apollo.tsx
+++ src/apollo.tsx
@@ -2,7 +2,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:3000/graphql', // GraphQL endpoint
+  uri: 'http://localhost:4000/graphql', // GraphQL endpoint
   cache: new InMemoryCache(),
 });
 
