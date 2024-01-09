797f07b62787d95bf8009de378e3dee1f3d47bea
Syed Wajahat Ali <wajahat.klk25.com>
2024-01-09 18:28:04 +0000

    fixes routes for home link in error page

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -9,7 +9,7 @@ export default function ErrorPage() {
         <div className="fof">
           <h1>Error 404</h1> <br></br>
           <h1>Page not found</h1>
-          <Link to="/home">
+          <Link to="/">
             <p className="mt-12 text-2xl text-violet-500 border-dashed border-2 border-violet-500 w-fit py-4 px-8 mx-auto hover:bg-violet-500 hover:text-white hover:text-2xl">
               TAKE ME HOME
             </p>
